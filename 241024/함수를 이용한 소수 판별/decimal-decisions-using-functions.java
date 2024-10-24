f230d3f0220dd7820fb8fe363666e31789cd81f5
ssem-i <115564717+ssem-i@users.noreply.github.com>
2024-10-24 15:24:58 +0900

    Title: 함수를 이용한 소수 판별, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 241024/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Main {
 
     public static boolean isPrime(int a){
+        if (a<2)return false;
         for(int i=2; i<=Math.sqrt(a); i++){
 
             if(a%i==0)
