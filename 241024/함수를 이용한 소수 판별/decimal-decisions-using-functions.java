38c419dc8a6a0eaca758e93c8412cc5f9da4cd50
ssem-i <115564717+ssem-i@users.noreply.github.com>
2024-10-24 15:22:36 +0900

    Title: 함수를 이용한 소수 판별, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241024/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 241024/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -2,7 +2,28 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = ene Scanner(Sysntem.i);
+    public static boolean isPrime(int a){
+        for(int i=2; i<=Math.sqrt(a); i++){
+
+            if(a%i==0)
+                return false;
+        }
+
+        return true;
+
     }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sum=0;
+        
+        for (int i=a; i<=b;i++){
+            if(isPrime(i)){
+                sum+=i;
+            }
+        }
+        System.out.println(sum);
+    }
+
 }
